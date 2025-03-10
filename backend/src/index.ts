3053954eeba6f84e18f390f05428672131fb99d8
Amirhossein <amir.hossein.dev9108@gmail.com>
2025-03-11 01:30:22 +0330

    message routes and controllers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.ts";
-import messageRoutes from "./routes/massage.route.ts";
+import messageRoutes from "./routes/message.route.ts";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 dotenv.config();
@@ -9,7 +9,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
-app.use("/api/massages", messageRoutes);
+app.use("/api/messages", messageRoutes);
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
--- backend/src/routes/massage.route.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-router.get("/conversation", (req, res) => {
-  res.send("Conversation route");
-});
-router.get("/message", (req, res) => {
-  res.send("Message route");
-});
-export default router;
